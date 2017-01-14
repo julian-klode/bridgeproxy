4745eb0d957a452322925447fec7ede5a0a72cfe
Julian Andres Klode <jak@jak-linux.org>
2017-01-14 22:42:41 +0100

    Do not hardcode expected CONNECT response

--- bridgeproxy.go
+++ bridgeproxy.go
@@ -7,17 +7,17 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 // Peer is a server we are connecting to. This can either be an
 // intermediate http(s) proxy server or the final server we want
 // to connect to.
 type Peer struct {
-	TLSConfig       *tls.Config // nil if unencrypted, valid config otherwise
-	HostName        string      // The hostname to connect to
-	Port            int         // The port to connect to on the hostname
-	ConnectExtra    string      // Extra headers to send after the CONNECT line
-	ConnectResponse string      // Response to expect from the server
+	TLSConfig    *tls.Config // nil if unencrypted, valid config otherwise
+	HostName     string      // The hostname to connect to
+	Port         int         // The port to connect to on the hostname
+	ConnectExtra string      // Extra headers to send after the CONNECT line
 }
 
 func forward(src io.ReadCloser, dst io.WriteCloser) {
@@ -26,6 +26,22 @@ func forward(src io.ReadCloser, dst io.WriteCloser) {
 	}
 	src.Close()
 	dst.Close()
+}
+
+// readLine reads a network line one byte at a time. We need to read unbuffered
+// as we might later turn the connection after a 200 response for a CONNECT
+// into a tls connection, for which we need a net.Conn.
+func readLine(src io.Reader) (string, error) {
+	line := make([]byte, 0, 64)
+	length := 0
+	for length < 2 || line[length-2] != '\r' || line[length-1] != '\n' {
+		line = append(line, 0)
+		if _, err := io.ReadFull(src, line[length:]); err != nil {
+			return "", err
+		}
+		length++
+	}
+	return string(line[:length]), nil
 }
 
 func handleRequest(client net.Conn, peers []Peer) {
@@ -42,14 +58,19 @@ func handleRequest(client net.Conn, peers []Peer) {
 			}
 		} else {
 			fmt.Fprintf(connection, "CONNECT %s:%d HTTP/1.0\r\n%s\r\n\r\n", peer.HostName, peer.Port, peers[i-1].ConnectExtra)
-			// Read the  and the 2 \r\n
-			expectedResponse := "HTTP/1.0 200 Connection established\r\n\r\n"
-			if peers[i-1].ConnectResponse != "" {
-				expectedResponse = peers[i-1].ConnectResponse
-			}
-			_, err = io.ReadFull(connection, make([]byte, len(expectedResponse)))
+
+			line, err := readLine(io.LimitReader(connection, 1024))
 			if err != nil {
 				fmt.Println("Could not read:", err)
+				return
+			}
+			if !strings.HasPrefix(line, "HTTP/1.0 200") && !strings.HasPrefix(line, "HTTP/1.1 200") {
+				client.Write([]byte("HTTP/1.0 502 Bad Gateway\r\nConnection: close\r\n\r\n"))
+				client.Write([]byte("Server error: " + line))
+				return
+			}
+			if line, err = readLine(connection); err != nil {
+				fmt.Println("Invalid second response line:", line)
 				return
 			}
 		}
